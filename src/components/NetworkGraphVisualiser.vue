d0f36df786a2f1d72b5909ed6308478965da7222
Medjahdi Islem <88147785+IslemMedjahdi@users.noreply.github.com>
2023-11-03 01:08:52 +0100

    fix alert

--- src/components/NetworkGraphVisualiser.vue
+++ src/components/NetworkGraphVisualiser.vue
@@ -5,7 +5,7 @@
         Articulation Point Calculator
       </p>
     </div>
-    <el-alert title="To select multiple edges, Shift+click" type="info" />
+    <el-alert title="To select multiple nodes, Shift+click" type="info" />
     <div
       class="flex items-center flex-wrap gap-2 bg-blue-50 px-4 py-2 border-blue-100"
     >
